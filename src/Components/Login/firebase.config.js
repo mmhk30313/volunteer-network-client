1fdc083dccf0ecc64429b3f7a066cbb7d5b93cd8
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2020-10-06 23:18:56 +0600

    Finally Alhumdulillah Done

--- src/Components/Login/firebase.config.js
+++ src/Components/Login/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBAU8mldWRyxNJkxYp092Ic5YDcsRmRayU",
-  authDomain: "real-volunteer-network.firebaseapp.com",
-  databaseURL: "https://real-volunteer-network.firebaseio.com",
-  projectId: "real-volunteer-network",
-  storageBucket: "real-volunteer-network.appspot.com",
-  messagingSenderId: "779627539160",
-  appId: "1:779627539160:web:1065a96eece8482a9ec5ad"
-};
+    apiKey: "AIzaSyBAU8mldWRyxNJkxYp092Ic5YDcsRmRayU",
+    authDomain: "real-volunteer-network.firebaseapp.com",
+    databaseURL: "https://real-volunteer-network.firebaseio.com",
+    projectId: "real-volunteer-network",
+    storageBucket: "real-volunteer-network.appspot.com",
+    messagingSenderId: "779627539160",
+    appId: "1:779627539160:web:1065a96eece8482a9ec5ad"
+  };
 export default firebaseConfig;
